ca98a7504d120eff86d172b0cdcef3bfc134c432
luis.mesa <luis.mesa@beeva.com>
2017-10-05 17:58:55 +0200

    Modularized frames generation operations

--- beeva-poc-ocr/controller/ffmpeg_controller.py
+++ beeva-poc-ocr/controller/ffmpeg_controller.py
@@ -7,18 +7,21 @@ class FFmpegController(object):
         self.__settings = settings
 
     def generate_frames_from_video(self, video_path, output_path, fr_threshold):
+        vid_name = os.path.splitext(os.path.basename(video_path))[0]
+        out_folder_abspath = os.path.join(os.path.abspath(output_path), vid_name)
+        if not os.path.exists(out_folder_abspath):
+            os.mkdir(out_folder_abspath)
+        out_abspath_with_frame_name = os.path.join(out_folder_abspath, self.__settings.FRAME_NAME_PATTERN)
+        cmd = self.__settings.FFMPEG_CMD.format(self.generate_quoted_path(os.path.abspath(video_path)), fr_threshold,
+                                                self.generate_quoted_path(out_abspath_with_frame_name))
         try:
-
-            vid_name = os.path.splitext(os.path.basename(video_path))[0]
-            out_folder_abspath = os.path.join(os.path.abspath(output_path), vid_name)
-            if not os.path.exists(out_folder_abspath):
-                os.mkdir(out_folder_abspath)
-            out_abspath_with_frame_name = os.path.join(os.path.abspath(out_folder_abspath), self.__settings.FRAME_NAME_PATTERN)
-            cmd = self.__settings.FFMPEG_CMD.format(self.generate_quoted_path(os.path.abspath(video_path)), fr_threshold,
-                                                    self.generate_quoted_path(out_abspath_with_frame_name))
-            return subprocess.check_call(cmd, shell=True)
+            exit_code = subprocess.check_call(cmd, shell=True)
+            if exit_code == 0:
+                return out_folder_abspath
+            else:
+                return None
         except subprocess.CalledProcessError:
-            return -1
+            return None
 
     @staticmethod
     def generate_quoted_path(path):
--- beeva-poc-ocr/poc_ocr.py
+++ beeva-poc-ocr/poc_ocr.py
@@ -25,15 +25,20 @@ class PoCOCR:
                 video_info = self.__youtube_dl_controller.get_info_from_youtube(video)
                 self.__youtube_dl_controller.download_from_youtube(video)
                 youtube_local_files = find_local_file("*{}.{}".format(video_info['id'], video_info['ext']), "./")
-                ffmpeg_exit = self.__ffmpeg_controller.generate_frames_from_video(youtube_local_files[0], output_path, threshold)
-                if ffmpeg_exit != 0:
-                    raise Exception("Failed while trying to generate frames from video")
+                output_folder = self.generate_frames_from_video(youtube_local_files[0], output_path, threshold)
                 os.remove(youtube_local_files[0])
             else:
-                self.__ffmpeg_controller.generate_frames_from_video(video, output_path, threshold)
+                output_folder = self.generate_frames_from_video(video, output_path, threshold)
         except Exception as ex:
             logging.exception("[PoC OCR] Exception: {}".format(ex))
             exit(-1)
+
+    def generate_frames_from_video(self, video_file, output_path, threshold):
+        output_folder = self.__ffmpeg_controller.generate_frames_from_video(video_file, output_path, threshold)
+        if not output_folder:
+            raise Exception("Failed while trying to generate frames from video")
+        else:
+            return output_folder
 
 
 def main():
